2c24ea79dcf7ff66ce0c5408df2431497f6ad6e8
awexli <awexli@mail.sfsu.edu>
2020-02-09 18:05:57 -0800

    initialize foundation

---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerText = "Helleo"
+
+    return element;
+}
+
+//document.getElementById("container").appendChild(component());
