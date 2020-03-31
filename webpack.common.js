c71e14aa6d307472d02273cabc924a06fa80de80
awexli <awexli@mail.sfsu.edu>
2020-03-30 22:47:17 -0700

    utilize clean-webpack-plugin
    - refactor ricepaper to be jpg

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,5 @@
 const path = require('path');
-//const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -8,7 +8,7 @@ module.exports = {
    },
    plugins: [
      // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
-     //new CleanWebpackPlugin(),
+     new CleanWebpackPlugin(),
      new HtmlWebpackPlugin({
        title: 'Production',
        template: './src/template.html'
