9c28c0f6028d6cd97c033982a72adbb8d0b8ad02
awexli <awexli@mail.sfsu.edu>
2020-04-04 21:14:08 -0700

    remove js generated html

---
+++ src/old/old-render-view.js
@@ -0,0 +1,12 @@
+// import header from "./header";
+// import options from "./options";
+// import footer from "./footer";
+
+// const fragment = new DocumentFragment();
+// const contents = [header.navbarFragment(), options.mainFragment(), footer.footerFragment()];
+
+// for (let item of contents) {
+//   fragment.appendChild(item);
+// }
+
+// document.querySelector(".wrapper").appendChild(fragment);
