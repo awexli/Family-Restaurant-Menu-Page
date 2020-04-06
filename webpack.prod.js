b6a564d82a9e5dcd4ac50894ea34da37d4858dd8
awexli <awexli@mail.sfsu.edu>
2020-04-06 00:56:27 -0700

    remove banner folder

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,7 +24,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
-    new CleanWebpackPlugin( { cleanOnceBeforeBuildPatterns: ['**/*','!banner/**', '!favi/**'],}),
+    new CleanWebpackPlugin( { cleanOnceBeforeBuildPatterns: ['**/*', '!favi/**'],}),
   ],
   module: {
     rules: [
