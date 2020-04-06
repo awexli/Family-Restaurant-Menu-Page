385726f3133996c80640c848b32edbe2b14dd00f
awexli <awexli@mail.sfsu.edu>
2020-04-06 00:40:04 -0700

    add banner img

--- webpack.prod.js
+++ webpack.prod.js
@@ -24,7 +24,7 @@ module.exports = merge(common, {
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: "[name].[contentHash].css" }),
-    new CleanWebpackPlugin( { cleanOnceBeforeBuildPatterns: ['**/*', '!favi/**'],}),
+    new CleanWebpackPlugin( { cleanOnceBeforeBuildPatterns: ['**/*','!banner/**', '!favi/**'],}),
   ],
   module: {
     rules: [
