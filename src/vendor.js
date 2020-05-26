1c33cb183166fbd94af9752484f66019157605e4
Alex Li <sf.alexli@gmail.com>
2020-05-26 03:04:54 -0700

    lint files

--- src/vendor.js
+++ src/vendor.js
@@ -1,8 +1,9 @@
-import "bootstrap/js/src/modal";
+import 'bootstrap/js/src/modal';
+
 (function (d, t) {
   var g = d.createElement(t);
   var s = d.getElementsByTagName(t)[0];
-  g.id = "yelp-biz-badge-script-plain-m5c-bIR-Y6h5ylUIbRDP7Q";
-  g.src = "//yelp.com/biz_badge_js/en_US/plain/m5c-bIR-Y6h5ylUIbRDP7Q.js";
+  g.id = 'yelp-biz-badge-script-plain-m5c-bIR-Y6h5ylUIbRDP7Q';
+  g.src = '//yelp.com/biz_badge_js/en_US/plain/m5c-bIR-Y6h5ylUIbRDP7Q.js';
   s.parentNode.insertBefore(g, s);
-})(document, "script");
+})(document, 'script');
